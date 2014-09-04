355b126a99b47af3176a1ea08b642350fca0c5f7
danicomas <danicomas@gmail.com>
2014-09-04 13:12:37 +0200

    Added JavaScript

---
+++ angular-jquery-datepicker.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+angular.module('angular-jquery-datepicker', []).factory('datepicker', [function () {
+    return $.datepicker;
+}]).provider('$datepicker', function () {
+    return {
+        setDefaults: function (language) {
+            $.datepicker.setDefaults($.datepicker.regional[language]);
+        },
+        $get: function () {
+            return {
+
+            }
+        }
+    }
+}).directive('datepicker', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attr, ctrl) {
+            elem.datepicker(
+                {
+                    showOn: "button",
+                    buttonImage: "http://www.ingenslabs.com/proassist/images/icons/Calendar.png",
+                    buttonImageOnly: true
+                });
+        }
+    };
+});
